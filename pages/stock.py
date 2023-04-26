8d0540e7f6eb36d9ac64f3518111261e1016b3a6
atelime <atelierlime@gmail.com>
2023-04-26 16:18:55 +0900

    Update stock.py

--- pages/stock.py
+++ pages/stock.py
@@ -47,8 +47,3 @@ if "거래량" in selected_chart:
    st.write("## 거래량")
    px_volumne = px.bar(tickerDf[["Volume"]])
    px_volumne
-
-if "전일비" in selected_chart:  
-   st.write("## 전일비")
-   px_change = px.area(tickerDf[["Change"]])
-   px_change
